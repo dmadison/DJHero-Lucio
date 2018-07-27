1ed0328801586e30a634993c650a8e431663b3d6
David Madison <dmadison@users.noreply.github.com>
2018-07-27 12:34:49 -0400

    Added startup lines for debug mode
    Just to add a bit of polish. It's a nice way to mark the start of the debug output.

--- Arduino/DJLucio/DJLucio.ino
+++ Arduino/DJLucio/DJLucio.ino
@@ -79,6 +79,10 @@ void setup() {
 	#ifdef DEBUG
 	Serial.begin(115200);
 	while (!Serial);  // Wait for connection
+	DEBUG_PRINTLN("#### DJ Hero - Lucio v?.? ####");
+	DEBUG_PRINTLN("By David Madison, (c) 2018");
+	DEBUG_PRINTLN("http://www.partsnotincluded.com");
+	DEBUG_PRINTLN("----------------------------");
 	#endif
 
 	pinMode(SafetyPin, INPUT_PULLUP);
